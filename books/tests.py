bb3afe7045ec1cb52fec755c342aee3f71ff0389
Alex's PC <zhangyh8216@sina.com>
2025-03-15 23:34:03 +0800

    ch12

--- books/tests.py
+++ books/tests.py
@@ -30,3 +30,23 @@ class BookTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Harry Potter")
         self.assertTemplateUsed(response, "books/book_detail.html")
+
+    def test_book_listing(self):
+        self.assertEqual(f"{self.book.title}", "Harry Potter")
+        self.assertEqual(f"{self.book.author}", "JK Rowling")
+        self.assertEqual(f"{self.book.price}", "25.00")
+
+    def test_book_list_view(self):
+        response = self.client.get(reverse("book_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Harry Potter")
+        self.assertTemplateUsed(response, "books/book_list.html")
+
+    def test_book_detail_view(self):
+        response = self.client.get(self.book.get_absolute_url())
+        no_response = self.client.get("/books/12345/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "Harry Potter")
+        self.assertContains(response, "An excellent review")  # new
+        self.assertTemplateUsed(response, "books/book_detail.html")
